ea00febc8ed7ac1b2eafca4dccfc71ee5e85ab4d
Animesh Patel <animeshx.patel@intel.com>
2015-04-17 20:54:42 +0530

    dev-1.x-TCB-protection-for-host: moved kernel arguments to measure_host.cfg file

--- tcb_protection/configure_host.sh
+++ tcb_protection/configure_host.sh
@@ -9,7 +9,7 @@ MENUENTRY_PREFIX="TCB-Protection"
 CREATE_MENU_ENTRY_SCRIPT="$BASE_DIR/create_menuentry.pl"
 UPDATE_MENU_ENTRY_SCRIPT="$BASE_DIR/update_menuentry.pl"
 GRUB_FILE=""
-
+CONFIG_FILE_NAME="/measure_host.cfg"
 
 function help_instruction()
 {
@@ -88,7 +88,7 @@ function get_partition_info()
 	PARTITION_INFO="" 
 	for val in `df -P -t ext4 -t ext3 -t ext2 | grep -i -v Filesystem | awk '{ print $1 ":" $6}'`
 	do 
-		PARTITION_INFO=$PARTITION_INFO","$val; 
+		PARTITION_INFO="${PARTITION_INFO},${val}" 
 	done 
 	PARTITION_INFO=`echo $PARTITION_INFO | cut -c2-`
 	PARTITION_INFO="{"$PARTITION_INFO"}"
@@ -98,8 +98,9 @@ function get_partition_info()
 function generate_kernel_args()
 {
 	echo "Following kernel argument will be used in grub menuentry for TCB Protection: "
-	KERNEL_ARGS="MANIFEST_PATH=\"`readlink -e $MANIFEST_PATH`\" PARTITION_INFO=\"$PARTITION_INFO\""
+	KERNEL_ARGS="MANIFEST_PATH=\"`readlink -e $MANIFEST_PATH`\"\nPARTITION_INFO=\"$PARTITION_INFO\""
 	echo $KERNEL_ARGS
+	echo -e $KERNEL_ARGS > $CONFIG_FILE_NAME
 	echo ""
 }
 
@@ -146,7 +147,7 @@ function generate_grub_entry()
 	echo > $MENUENTRY_FILE
 	which_grub
 	get_grub_file_location
-	perl $CREATE_MENU_ENTRY_SCRIPT $MENUENTRY_FILE $(uname -r) "$INITRD_NAME" "$KERNEL_ARGS" "$MENUENTRY_PREFIX" "$GRUB_FILE" $GRUB_VERSION 
+	perl $CREATE_MENU_ENTRY_SCRIPT $MENUENTRY_FILE $(uname -r) "$INITRD_NAME" "$MENUENTRY_PREFIX" "$GRUB_FILE" $GRUB_VERSION 
 	if [ $? -ne 0 ]; then
 		echo "ERROR: Not able to get appropriate grub entry from $GRUB_FILE file for kernel version $KERNEL_VERSION ."
 		echo "For Ubuntu OS make sure that tboot is available on the host and for current kernel tboot entry is populated in $GRUB_FILE file."
@@ -158,7 +159,6 @@ function generate_grub_entry()
 function update_grub()
 {
 	if [ "$GRUB_VERSION" == "0" ]; then
-		echo "$GRUB_FILE already contains an entry for TCB-Protection"
 		echo "Updating the $GRUB_FILE with newly generated entry"
 		perl $UPDATE_MENU_ENTRY_SCRIPT $MENUENTRY_FILE $GRUB_FILE $GRUB_VERSION $MENUENTRY_PREFIX
 		if [ $? -ne 0 ]
